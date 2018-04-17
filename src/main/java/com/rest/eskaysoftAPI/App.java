fd24cf66b16453ae22791c57e8b65d4dc4fb2801
Siva Kumar G <skumar.gubbala@gmail.com>
2018-04-17 15:49:07 +0530

    Added context.xml file to configure the Database JNDI

--- src/main/java/com/rest/eskaysoftAPI/App.java
+++ src/main/java/com/rest/eskaysoftAPI/App.java
@@ -2,14 +2,16 @@ package com.rest.eskaysoftAPI;
 
 import javax.sql.DataSource;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -18,7 +20,6 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAutoConfiguration
-@ComponentScan(basePackages = {"com.rest.eskaysoftAPI.service", "com.rest.eskaysoftAPI.service.impl", "com.rest.com.rest.eskaysoftAPI.resource"})
 @EnableJpaRepositories("com.rest.eskaysoftAPI.dao")
 @EntityScan(basePackages = "com.rest.eskaysoftAPI.entity")
 public class App extends SpringBootServletInitializer {
@@ -29,6 +30,18 @@ public class App extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(App.class);
+	}
+
+	@Bean
+	public TomcatEmbeddedServletContainerFactory tomcatFactory() {
+	    return new TomcatEmbeddedServletContainerFactory() {
+	        @Override
+	        protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(
+	                Tomcat tomcat) {
+	            tomcat.enableNaming();
+	            return super.getTomcatEmbeddedServletContainer(tomcat);
+	        }
+	    };
 	}
 
 	@Bean
